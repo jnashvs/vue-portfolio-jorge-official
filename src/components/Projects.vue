d9936a37309b5b8da7441dcab93f2751b7b6df4e
Jorge Silva <jnashvs@gmail.com>
2020-09-05 18:52:07 +0100

    fix bug in project 2.0

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -12,7 +12,7 @@
             in a piece of classical Latin literature from 45 BC, making it over
             2000 years old.
           </p>
-        </div>
+        </div> 
       </div>
       
       <div
