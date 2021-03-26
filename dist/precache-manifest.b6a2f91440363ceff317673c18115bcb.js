61df1037ae74e2695210055b548354eb60fd9a70
jnashvs <jnashvs@gmail.com>
2021-03-26 19:08:29 +0000

    run deploy to netlify

---
+++ dist/precache-manifest.b6a2f91440363ceff317673c18115bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc4b375c211dabad92e",
+    "url": "/css/app.72fd3268.css"
+  },
+  {
+    "revision": "dfda6ab03e341d742601c7833623d1a6",
+    "url": "/img/jorge.jpg"
+  },
+  {
+    "revision": "7b95393e4741dc7b792dae84a86d7761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cc4b375c211dabad92e",
+    "url": "/js/app.047bfc79.js"
+  },
+  {
+    "revision": "4b76b12d0dbe0668fa3a",
+    "url": "/js/chunk-vendors.8c4bf7e2.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
