1d5dcadda00edccbea8cb8c7311fa2ecc848c686
jnashvs <jnashvs@gmail.com>
2021-03-26 18:40:23 +0000

    deploy

---
+++ precache-manifest.c78f0c45f6f825204d8d3d0f57950a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0150a2cc741a5f01a0",
+    "url": "/vue-portfolio/css/app.72fd3268.css"
+  },
+  {
+    "revision": "dfda6ab03e341d742601c7833623d1a6",
+    "url": "/vue-portfolio/img/jorge.jpg"
+  },
+  {
+    "revision": "752e6e551b0ce07b3dac4734981d2a8f",
+    "url": "/vue-portfolio/index.html"
+  },
+  {
+    "revision": "ce0150a2cc741a5f01a0",
+    "url": "/vue-portfolio/js/app.1b2ab8db.js"
+  },
+  {
+    "revision": "4b76b12d0dbe0668fa3a",
+    "url": "/vue-portfolio/js/chunk-vendors.8c4bf7e2.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/vue-portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-portfolio/robots.txt"
+  }
+]);
